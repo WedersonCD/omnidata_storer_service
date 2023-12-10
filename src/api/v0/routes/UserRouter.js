db0428cd5bf06443c1b36f7e29118563000e4f63
Wederson Cabral Domingues <wederson.domingues@stz.com.br>
2023-12-10 07:09:20 -0300

    Add get users end-point

--- src/api/v0/routes/UserRouter.js
+++ src/api/v0/routes/UserRouter.js
@@ -2,6 +2,8 @@ const UserController = require('../controllers/UserController');
 const router = require('express').Router();
 
 router.post('/',UserController.createUser);
+router.get('/',UserController.getAllUsers);
+router.get('/:user_id',UserController.getUserById);
 
 
 module.exports = router;
