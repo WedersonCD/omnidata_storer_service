733c267027f2e8783f572ebf0731b2df48048755
Wederson Cabral Domingues <wederson.domingues@stz.com.br>
2023-12-10 09:09:29 -0300

    Create Data Product Controller

---
+++ src/api/v0/controllers/DataProductController.js
@@ -0,0 +1,47 @@
+const DataProductModel = require('../models/DataProduct');
+
+const DataProductController = {}
+
+DataProductController.getAllDataProducts = async (req, res) => {
+    try {
+        const dataProducts = await DataProductModel.find().populate('user_owner');
+        res.status(200).json(dataProducts);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+DataProductController.getDataProductById = async (req, res) => {
+    try {
+        const dataProductId = req.params.dataProduct_id;
+        const dataProduct = await DataProductModel.findById(dataProductId).populate('user_owner');
+
+        if (!dataProduct) {
+            return res.status(404).json({ message: 'Data Product not found' });
+        }
+
+        res.status(200).json(dataProduct);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+DataProductController.createDataProduct = async (req, res) => {
+    
+    const newDataProduct = new DataProductModel({
+        dataProduct_name: req.body.dataProduct_name,
+        dataProduct_dominium: req.body.dataProduct_dominium,
+        user_owner: req.body.user_owner
+    });
+
+    try {
+        const savedDataProduct = await newDataProduct.save();
+        res.status(201).json(savedDataProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+
+module.exports = DataProductController;
